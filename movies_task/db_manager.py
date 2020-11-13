9bafd050c5a72bb953469fbd08287da8133a1bd4
ldaijiw <lwaltmann@spartaglobal>
2020-11-13 16:10:42 +0000

    started 2nd iteration to incorporate text files

--- movies_task/db_manager.py
+++ movies_task/db_manager.py
@@ -7,7 +7,14 @@ class MovieDBManager:
         self.user = 'daiji'
         self.start_connection()
 
-    
+
+    @classmethod
+    def from_existing_table(cls, table_name):
+        new_class = cls()
+        new_class.table_name = table_name
+        return new_class
+
+
     def start_connection(self, server = "databases1.spartaglobal.academy", database = "Northwind", username = "SA", password = "Passw0rd2018"):
         try:
             print("Establishing Connection")
@@ -21,6 +28,7 @@ class MovieDBManager:
         else:
             # if no errors occurs prints following message
             print("Connection Successfully Made")
+            self.database = database
 
 
     def create_table(self):
@@ -130,43 +138,65 @@ class MovieDBManager:
         self.cursor.execute(insert_data_query)
 
 
-
-
-    def data_from_file(self, filename):
+    def data_from_file(self, filename, insert_data = False):
         with open(filename, "r") as datafile:
             if filename[-3:] == "csv":
                 csvdata = list(csv.reader(datafile))
                 table_name = f"{self.user}_{filename[:-4].split('/')[-1]}"
                 return table_name, csvdata
+            
+            elif filename[-3:] == "txt":
+                textdata_raw = datafile.read().replace(', ', ',').splitlines()
+                textdata = [item.split(',') for item in textdata_raw]
                 
-                
-                
+                if insert_data:
+                    print("INSERTING DATA FROM TEXT FILE")
+                    for item in textdata:
+                        self.insert_data(*item)
+                        
+                return textdata
 
 
 
-    def table_info(self):
-        pass
+    @property
+    def table_information(self):
+        table_info = self.cursor.execute(f'''
+        SELECT
+        TABLE_NAME,
+        COLUMN_NAME
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_NAME = '{self.table_name}';
+        ''').fetchall()
+        return table_info
+        
 
 
     def select_query(self, select_details = "*"):
-        query = f"SELECT {select_details} FROM {self.table_name}"
-
-        return self.cursor.execute(query).fetchall()
+        query = f"SELECT {select_details} FROM {self.table_name};"
+        print(query)
+        return self.cursor.execute(query).fetchall()[0]
 
     def find_movie(self, movie_title):
+        query = f"SELECT * FROM {self.table_name} WHERE primaryTitle IS {movie_title}"
         pass
 
-    
-    def add_movie(self, movie_details):
-        pass
+
+    def delete_table(self, table_name):
+        query = f"DROP TABLE {table_name};"
+        self.cursor.execute(query)
+
 
 
 def main():
     new_mdbm = MovieDBManager()
-    #new_mdbm.data_from_file("movies_task/imdbtitles.csv")
+    # new_mdbm.delete_table("daiji_imdbtitles")
     new_mdbm.create_table()
-    #new_mdbm.insert_data('action', 'inception', 'inception', 'y', '2019', '2020', '120', 'action')
-    print(new_mdbm.select_query("COUNT(*)"))
+    new_mdbm.data_from_file("movies_task/more_movies.txt", insert_data=True)
+    # print(new_mdbm.table_name)
+    # print(new_mdbm.table_information)
+
+   
+    #print(new_mdbm.select_query())
 
 
 if __name__ == "__main__":
